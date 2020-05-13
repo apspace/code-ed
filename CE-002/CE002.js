650d3d832ee7c5bcf3ba62a9483c7fe5944348d4
Anton Popov <apwork03@gmail.com>
2020-05-13 21:11:54 +0300

    t

--- CE-002/CE002.js
+++ CE-002/CE002.js
@@ -1,16 +1,33 @@
-let pink;
-let c, img, k;
-let slider, button;
+let canvas, img, k, n, m;
+let r, g, b;
+let slider1, slider2, button;
 
 function setup() {
-	c = createCanvas(windowWidth, windowHeight);
-	c.position(0, 0);
-	c.style('z-index', ' -1');
-	img = createImage(windowWidth, windowHeight);
-	slider = createSlider(0, 100, 30);
-	slider.style('width', '210px');
-	slider.class('slider');
-	slider.changed(update);
+	canvas = createCanvas(500, 500);
+	canvas.style('border-radius', '30px');
+
+	// canvas.position(0, 0);
+	// canvas.style('z-index', ' -1');
+	img = createImage(540, 540);
+	pixelDensity(2);
+
+	slider1 = createSlider(0, 200, 50);
+	slider1.style('width', '210px');
+	slider1.style('display', 'block');
+	slider1.class('slider');
+	slider1.changed(update);
+
+	slider2 = createSlider(0, 100, 5);
+	slider2.style('width', '210px');
+	slider2.style('display', 'block');
+	slider2.class('slider');
+	slider2.changed(update);
+
+	button = createButton('Save PNG');
+	button.mousePressed(saveImg);
+	button.style('width', '210px');
+	button.style('height', '30px');
+
 	k = PI / 180;
 	background(120);
 
@@ -18,20 +35,25 @@ function setup() {
 function draw() {
 	update();
 	noLoop();
-
 }
 
 function update() {
 	img.loadPixels();
 	for (let i = 0; i < img.width; i++) {
 		for (let j = 0; j < img.height; j++) {
-			let m = cos(i * 0.0045) * tan(slider.value() / 10 * k);
-			let r = 127 + 127 * sin(m * i * j * 0.001);
-			let g = 127 + 127 * sin(m * i * j * 0.00103);
-			let b = 127 + 127 * sin(m * i * j * 0.00106);
+			m = (cos(i * 0.0045) * slider1.value()) / 100000;
+			n = slider2.value() / 10;
+
+			let r = 127 + 127 * sin(m * i * j);
+			let g = 127 + 127 * sin(m * i * j + n);
+			let b = 127 + 127 * sin(m * i * j + 2 * n);
 			img.set(i, j, color(r, g, b));
 		}
 	}
 	img.updatePixels();
 	image(img, 0, 0);
+}
+
+function saveImg() {
+	saveCanvas('CE002', 'png');
 }
